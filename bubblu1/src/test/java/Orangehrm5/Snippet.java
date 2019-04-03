7445a79373921c8781c1f433a46b0bc72123800c
Venki <Venki@Venki-PC>
2019-04-03 13:26:26 +0530

    bubblu1

---
+++ bubblu1/src/test/java/Orangehrm5/Snippet.java
@@ -0,0 +1,65 @@
+package Orangehrm5;
+
+public class Snippet {
+	 <dependencies>
+	   <dependency>
+	        <groupId>org.seleniumhq.selenium</groupId>
+	        <artifactId>selenium-java</artifactId>
+	        <version>3.141.59</version>
+	    </dependency>  
+	    
+	      <dependency>
+	             <groupId>org.apache.poi</groupId>
+	            <artifactId>poi-ooxml</artifactId>
+	            <version>3.17</version>
+	       </dependency>
+	       
+		    <dependency>
+		    <groupId>org.testng</groupId>
+		    <artifactId>testng</artifactId>
+		    <version>6.14.3</version>
+		    <scope>test</scope>
+			</dependency>
+	    	
+	    	<dependency>
+			  <groupId>org.testng</groupId>
+			  <artifactId>testng</artifactId>
+			  <version>6.14.2</version>
+			  <scope>test</scope>
+			</dependency>
+			
+			<dependency>
+		    <groupId>org.apache.commons</groupId>
+		    <artifactId>commons-io</artifactId>
+		    <version>1.3.2</version>
+			</dependency>
+			
+		    <dependency>
+		    <groupId>log4j</groupId>
+		    <artifactId>log4j</artifactId>
+		    <version>1.2.17</version>
+			</dependency>
+			
+			   <!-- always needed -->     
+				<!-- https://mvnrepository.com/artifact/com.itextpdf/itext7-core -->
+			<!--
+			<dependency>
+			    <groupId>com.itextpdf</groupId>
+			    <artifactId>itext7-core</artifactId>
+			    <version>7.1.4</version>
+			    <type>pom</type>
+			</dependency>  -->     
+			
+			<dependency>
+	    	<groupId>com.itextpdf</groupId>
+	    	<artifactId>itextpdf</artifactId>
+	   		 <version>5.0.6</version>
+			</dependency>
+				
+	       <!-- always needed --> 
+	  </dependencies>
+	
+	</project>
+	
+}
+
